d225e89619ec108f5bfa62256b846d70acd6f79d
Cecilia Edvardsson <122752507+ceed2200@users.noreply.github.com>
2024-01-21 10:47:54 +0100

    alla filer

---
+++ Del1/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Byte av textfärg vid mouseover
+
+document
+  .getElementById("date")
+  .addEventListener("mouseover", changeColor, false);
+
+function changeColor(e) {
+  if (e.target.style.color == "black") {
+    e.target.style.color = "darkblue";
+  } else {
+    e.target.style.color = "black";
+  }
+}
